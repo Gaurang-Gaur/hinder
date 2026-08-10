b33b7f465fb0f66c2e4b699210451fda7c4056a2
Gaurang Gaur <gauranggaur007@gmail.com>
2026-08-11 02:59:23 +0530

    add shared folder

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { ErrorComponent } from './shared/components/error/error.component.js';
 import { LoginComponent } from './features/login/login.component.js';
 import { DashboardComponent } from './features/dashboard/dashboard.component.js';
+import { authGuard } from './core/guards/auth-guard.js';
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', canActivate: [authGuard], component: DashboardComponent },
   { path: '**', component: ErrorComponent },
 ];
---
+++ client/src/app/core/guards/auth-guard.ts
@@ -0,0 +1,11 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service.js';
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const isAuth = inject(AuthService);
+  if (isAuth) {
+    return true;
+  }
+  return router.createUrlTree(['']);
+};
