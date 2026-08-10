54edfb7ca594905fb45b273ebcb168d4226e3689
Gaurang Gaur <gauranggaur007@gmail.com>
2026-08-11 02:31:58 +0530

    added navigation to dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ErrorComponent } from './shared/components/error/error.component.js';
 import { LoginComponent } from './features/login/login.component.js';
+import { DashboardComponent } from './features/dashboard/dashboard.component.js';
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: '*', component: ErrorComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: ErrorComponent },
 ];
--- client/src/app/features/login/login.component.ts
+++ client/src/app/features/login/login.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  router = inject(Router);
+  handleSubmit(formValue: NgForm) {
+    console.log(formValue.value.email + formValue.value.password, ' i am passed');
+    this.router.navigate(['/dashboard']);
+  }
+}
